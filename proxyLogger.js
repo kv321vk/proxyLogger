c7296cac3b30c7dd966ad36553b40abd9c1d8db2
k <1602111431@qq.com>
2017-09-22 14:20:29 +0800

    Update proxyLogger.js

--- proxyLogger.js
+++ proxyLogger.js
@@ -1,1 +1,12 @@
-
+export const logConfigerIgnore = (ignoreArr) => {
+  window.console = new Proxy( window.console, {
+    get( target, key) {
+      return function(...args) {
+        if (ignoreArr.includes(key)) {
+          return
+        }
+        return Reflect.apply(target[key], target, args);
+      }
+    }
+  })
+}
